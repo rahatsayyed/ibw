9aaffa48649342fe2b271a439f4739b12e247000
Rahat Sayyed <70068895+rahatsayyed@users.noreply.github.com>
2025-11-29 17:10:33 +0530

    feat: Introduce Plutus validator JSON and a script to generate TypeScript exports.

---
+++ generatePlutusTS.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+
+// Path to the JSON file
+const jsonPath = path.join(__dirname, "./ibw/plutus.json");
+
+// Load the JSON content
+const jsonContent = require(jsonPath);
+
+// Prepare an array to hold export statements
+const validatorexports = [];
+
+jsonContent.validators.forEach((validator) => {
+  const title = validator.title.replace(/\./g, "_"); // Replace dots with underscores
+  const compiledCode = validator.compiledCode;
+  if (compiledCode) {
+    validatorexports.push(
+      `export const ${title} = ${JSON.stringify(compiledCode)};`
+    );
+  }
+});
+
+// Write the JS file
+const outputPath = path.join(__dirname, "./web/config/scripts/plutus.ts");
+fs.writeFileSync(outputPath, validatorexports.join("\n"), "utf8");
+console.log("plutus.js generated successfully!");
